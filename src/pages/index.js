412a371453526470ce415570f53b7ac8cbf7cf45
imthebest9 <lookai0303@gmail.com>
2021-12-07 21:19:50 +0800

    init

---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "sections/banner";
+import Faq from "sections/faq";
+import Testimonials from "sections/testimonials";
+import Security from "sections/security";
+import Addons from "sections/addons";
+import MobileApp from "sections/mobile-app";
+import Dashboard from "sections/dashboard";
+import UltimateFeatures from "sections/ultimate-features";
+import Link from "next/link";
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO
+          title="Recipifier"
+          description="Food Recipe Recommendation Engine For Everyone "
+        />
+        <Banner />
+        <h1 className="title">
+          Read{" "}
+          <Link href="/secondpage">
+            <a>this page!</a>
+          </Link>
+        </h1>
+        <Testimonials />
+        <Security />
+        <Addons />
+        <Dashboard />
+        <UltimateFeatures />
+        <MobileApp />
+        <Faq />
+      </Layout>
+    </ThemeProvider>
+  );
+}
